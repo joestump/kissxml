52c1bb812d2220d7475a2cae5d52713325e3ce7e
Joe Stump <joe@joestump.net>
2009-08-31 20:13:17 -0600

    Removed tests directory and added support for mulitple nodes with the same name.

--- simplexml.py
+++ simplexml.py
@@ -9,10 +9,21 @@ class XMLTree(object):
     def __init__(self, node):
         self.node = node
         for n in node:
-            if len(n.getchildren()):
-                self.nodes[n.tag] = XMLTree(n)
+            if n.tag in self.nodes:
+                if len(self.nodes[n.tag]) == 1:
+                    self.nodes[n.tag] = [self.nodes[n.tag]]
             else:
-                self.nodes[n.tag] = XMLNode(n)
+                self.nodes[n.tag] = None
+
+            if len(n.getchildren()):
+                wrapper = XMLTree
+            else:
+                wrapper = XMLNode
+
+            if self.nodes[n.tag] != None and len(self.nodes[n.tag]):
+                self.nodes[n.tag].append(wrapper(n))
+            else:
+                self.nodes[n.tag] = wrapper(n)
 
     def __getattr__(self, attr):
         return self.nodes[attr]
@@ -36,6 +47,21 @@ class XMLNode(object):
     def __repr__(self):
         return self.__unicode__()
 
+    def __len__(self):
+        return 1
+
 def parse(file):
     tree = ET.parse(file)
     return XMLTree(tree.getroot())
+
+if __name__ == "__main__":
+    xml = parse("fixture.xml")
+    print xml.name
+    print xml.hair["style"] 
+    print xml.like
+
+    print len(xml.name)
+    print len(xml.like)
+
+    for like in xml.like:
+        print "I like %s." % like
