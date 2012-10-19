98b6a2a7ab204d84ea46964f6b820c21cf0f5754
Jayson Reis <santosdosreis@gmail.com>
2012-10-19 17:02:05 -0300

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='python-simplexml',
+    version='0.1',
+    author='Joseph C. Stump',
+    author_email='joe@stu.mp',
+    description='A very simple XML manipulation.',
+    url='https://github.com/joestump/python-simplexml',
+    packages=['simplexml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
